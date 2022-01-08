e9d2fe26a6ec72df874fcf52a86d95ec950bed56
beafshar <baharafshar2079@gmail.com>
2022-01-09 02:14:13 +0330

    break handel offer into smaller functions to increase modularity. discount computation was moved to offer class.

--- java/src/main/java/dojo/supermarket/model/Offer.java
+++ java/src/main/java/dojo/supermarket/model/Offer.java
@@ -11,6 +11,31 @@ public class Offer {
         this.product = product;
     }
 
+
+    public Discount handel_offer_discount(Product p, double quantity, double unitPrice){
+        if (this.offerType == SpecialOfferType.TenPercentDiscount){
+            return new Discount(p, this.argument + "% off", -quantity * unitPrice * this.argument / 100.0);
+        }
+        else if (this.offerType == SpecialOfferType.ThreeForTwo){
+            return Calculate_Discount(p, 3, 2, unitPrice, (int)quantity, unitPrice);
+        }
+        else if (this.offerType == SpecialOfferType.TwoForAmount){
+            return Calculate_Discount(p, 2, this.argument, 1, (int)quantity, unitPrice);
+        }
+        else if (this.offerType == SpecialOfferType.FiveForAmount){
+            return Calculate_Discount(p, 5, this.argument, 1, (int)quantity, unitPrice);
+        }
+        return null;
+    }
+
+    Discount Calculate_Discount(Product p, int bought_products, double offer_amount, double price_coefficient, int quantity, double unitPrice){
+            int bought_products_ratio = quantity / bought_products;
+            if (quantity >= bought_products) {
+                double discountTotal = unitPrice * quantity - (offer_amount * bought_products_ratio * price_coefficient + quantity % bought_products * unitPrice);
+                return new Discount(p, bought_products  + " for " + offer_amount, -discountTotal);
+            }
+            return null;
+    }
     Product getProduct() {
         return this.product;
     }
--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -39,40 +39,11 @@ public class ShoppingCart {
             if (offers.containsKey(p)) {
                 Offer offer = offers.get(p);
                 double unitPrice = catalog.getUnitPrice(p);
-                int quantityAsInt = (int) quantity;
-                Discount discount = null;
-                int bought_products = 1;
-                int bought_products_ratio = quantityAsInt / bought_products;
-                
-                if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
-                    discount = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
-                }
-                else {
-                    double offer_amount = 0;
-                    double price_coefficient = 1;
-                    if (offer.offerType == SpecialOfferType.ThreeForTwo){
-                        bought_products  = 3;
-                        offer_amount = 2;
-                        price_coefficient = unitPrice;
-                    }
-                    else if (offer.offerType == SpecialOfferType.TwoForAmount) {
-                        bought_products = 2;
-                        offer_amount = offer.argument;
-                    }
-                    else if (offer.offerType == SpecialOfferType.FiveForAmount) {
-                        bought_products  = 5;
-                        offer_amount = offer.argument;
-                    }
-                    if (quantityAsInt >= bought_products) {
-                        double discountTotal = unitPrice * quantity - (offer_amount * bought_products_ratio * price_coefficient + quantityAsInt % bought_products * unitPrice);
-                        discount = new Discount(p, bought_products  + " for " + offer_amount, -discountTotal);
-                    }
-                }
-
+                Discount discount = offer.handel_offer_discount(p, quantity, unitPrice);
                 if (discount != null)
                     receipt.addDiscount(discount);
             }
-
         }
     }
+
 }
